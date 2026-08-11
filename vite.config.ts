fcde47b4c2d7c9eed45efa0c4f5bd1b242317d48
StackNi <643824428@qq.com>
2026-08-12 01:19:24 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/logo-bluearchive-colormode/',
   build: {
     rollupOptions: {
       output: {
