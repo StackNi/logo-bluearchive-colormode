636a288432f92ef81f83bf681392464a8bfb6a02
StackNi <643824428@qq.com>
2026-08-12 03:28:35 +0800

    Remove input configuration from Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: '/logo-bluearchive-colormode/',
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
       output: {
         assetFileNames: 'assets/ba-logo-[name]-[hash][extname]',
         entryFileNames: 'assets/ba-logo-[name]-[hash].js',
