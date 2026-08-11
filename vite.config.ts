095a14a5f83b8b39f6e6e7eda8d4b2dcdf1f9128
StackNi <643824428@qq.com>
2026-08-12 02:25:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/logo-bluearchive-colormode/',
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
+      output: {
+        assetFileNames: 'assets/ba-logo-[name]-[hash][extname]',
+        entryFileNames: 'assets/ba-logo-[name]-[hash].js',
+        chunkFileNames: 'assets/ba-logo-[name]-[hash].js',
       },
     },
   },
